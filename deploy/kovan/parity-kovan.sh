80c507af0a80a2c58263e000bb8ce878a2675d6b
Fil Capiral <24193763+filcap@users.noreply.github.com>
2018-05-15 15:22:45 -0700

    clean up kovan script

--- deploy/kovan/parity-kovan.sh
+++ deploy/kovan/parity-kovan.sh
@@ -2,7 +2,4 @@
 #set -e
 #set -o pipefail
 
-if [ -z ${DATA_DIR+x} ]; then DATA_DIR=/atonomi/demochain; fi
-echo -e "DATA_DIR is set to '$DATA_DIR'"
-
 parity --chain kovan  --geth --jsonrpc-apis "eth,net,parity,parity_pubsub,pubsub,rpc,shh,shh_pubsub,traces,web3" --jsonrpc-port 8545 --jsonrpc-interface local --jsonrpc-cors=none --no-warp
