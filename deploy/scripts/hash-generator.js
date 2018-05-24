54dfecd1af49c0a852af95abf91d020338329774
Fil Capiral <24193763+filcap@users.noreply.github.com>
2018-05-23 21:34:40 -0700

    update hash script

--- deploy/scripts/hash-generator.js
+++ deploy/scripts/hash-generator.js
@@ -5,11 +5,14 @@ const Web3 = require('web3')
 
 const nodeUrl = params[0]
 console.log('node:', nodeUrl)
+const web3 = new Web3(nodeUrl)
 
 const text = params[1]
-console.log('text:', text)
+console.log('ascii:', text)
 
-const web3 = new Web3(nodeUrl)
-const hash = web3Utils.soliditySha3({t: 'bytes32', v: web3.fromAscii(text)})
+const hexText = web3.fromAscii(text)
+console.log('hex', hexText)
+
+const hash = web3Utils.soliditySha3({t: 'bytes32', v: hexText})
 
 console.log('hash:', hash)
