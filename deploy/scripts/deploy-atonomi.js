51c6a0fb1381ba2aac29ce8962cf451d6c675c04
Fil Capiral <24193763+filcap@users.noreply.github.com>
2018-07-12 13:59:17 -0700

    Add helper to view parity pending txns

--- deploy/scripts/deploy-atonomi.js
+++ deploy/scripts/deploy-atonomi.js
@@ -198,3 +198,16 @@ function getAtonomiState(chain) {
   console.log('Reputation Author Share', reputationAuthorShare + '%')
   console.log('Manufacturer Share', (100 - reputationAuthorShare) + '%')
 }
+
+function getParityPendingTransactions(account, cb) {
+  web3.currentProvider.sendAsync({
+    jsonrpc: '2.0',
+    method: 'parity_pendingTransactions',
+    params: [],
+    id: 1
+  }, function(err, results) {
+    cb(err, results.result.filter(function(item) {
+      return item.from === account
+    }))
+  })
+}
