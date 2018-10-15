a40c89b8ca17347dcab2f16c616ab2d868f2d5ca
Fil Capiral <fil.capiral@atonomi.io>
2018-10-15 15:40:28 -0700

    Lint clean up on eternal storage contract

--- contracts/Storage/AtonomiEternalStorage.sol
+++ contracts/Storage/AtonomiEternalStorage.sol
@@ -2,66 +2,22 @@ pragma solidity ^0.4.23; // solhint-disable-line
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
-/// @title Atonomi persistent Key Value Store
 
 contract AtonomiEternalStorage is Ownable {
+    ///
+    /// Storage Types
+    ///
+    mapping(bytes32 => uint256) private uIntStorage;
+    mapping(bytes32 => string) private stringStorage;
+    mapping(bytes32 => address) private addressStorage;
+    mapping(bytes32 => bytes) private bytesStorage;
+    mapping(bytes32 => bytes32) private bytes32Storage;
+    mapping(bytes32 => bool) private boolStorage;
+    mapping(bytes32 => int256) private intStorage;
 
-
-    /**** Storage Types *******/
-
-    mapping(bytes32 => uint256)  private uIntStorage;
-    mapping(bytes32 => string)   private stringStorage;
-    mapping(bytes32 => address)  private addressStorage;
-    mapping(bytes32 => bytes)    private bytesStorage;
-    mapping(bytes32 => bytes32)    private bytes32Storage;
-    mapping(bytes32 => bool)     private boolStorage;
-    mapping(bytes32 => int256)   private intStorage;
-
-    
-    /*** Modifiers ************/
-
-
-    /**** Get Methods ***********/
-
-    /// @param _key The key for the record
-    function getAddress(bytes32 _key) external view returns (address) {
-        return addressStorage[_key];
-    }
-
-    /// @param _key The key for the record
-    function getUint(bytes32 _key) external view returns (uint) {
-        return uIntStorage[_key];
-    }
-
-    /// @param _key The key for the record
-    function getString(bytes32 _key) external view returns (string) {
-        return stringStorage[_key];
-    }
-
-    /// @param _key The key for the record
-    function getBytes(bytes32 _key) external view returns (bytes) {
-        return bytesStorage[_key];
-    }
-
-    /// @param _key The key for the record
-    function getBytes32(bytes32 _key) external view returns (bytes32) {
-        return bytes32Storage[_key];
-    }
-
-    /// @param _key The key for the record
-    function getBool(bytes32 _key) external view returns (bool) {
-        return boolStorage[_key];
-    }
-
-    /// @param _key The key for the record
-    function getInt(bytes32 _key) external view returns (int) {
-        return intStorage[_key];
-    }
-
-
-    /**** Set Methods ***********/
-
-
+    ///
+    /// Set Methods
+    ///
     /// @param _key The key for the record
     function setAddress(bytes32 _key, address _value) external {
         addressStorage[_key] = _value;
@@ -82,7 +38,7 @@ contract AtonomiEternalStorage is Ownable {
         bytesStorage[_key] = _value;
     }
 
-        /// @param _key The key for the record
+    /// @param _key The key for the record
     function setBytes32(bytes32 _key, bytes32 _value) external {
         bytes32Storage[_key] = _value;
     }
@@ -97,8 +53,9 @@ contract AtonomiEternalStorage is Ownable {
         intStorage[_key] = _value;
     }
 
-    /**** Delete Methods ***********/
-    
+    ///
+    /// Delete Methods
+    ///
     /// @param _key The key for the record
     function deleteAddress(bytes32 _key) external {
         delete addressStorage[_key];
@@ -132,5 +89,43 @@ contract AtonomiEternalStorage is Ownable {
     /// @param _key The key for the record
     function deleteInt(bytes32 _key) external {
         delete intStorage[_key];
+    }
+
+    ///
+    /// Get Methods
+    ///
+    /// @param _key The key for the record
+    function getAddress(bytes32 _key) external view returns (address) {
+        return addressStorage[_key];
+    }
+
+    /// @param _key The key for the record
+    function getUint(bytes32 _key) external view returns (uint) {
+        return uIntStorage[_key];
+    }
+
+    /// @param _key The key for the record
+    function getString(bytes32 _key) external view returns (string) {
+        return stringStorage[_key];
+    }
+
+    /// @param _key The key for the record
+    function getBytes(bytes32 _key) external view returns (bytes) {
+        return bytesStorage[_key];
+    }
+
+    /// @param _key The key for the record
+    function getBytes32(bytes32 _key) external view returns (bytes32) {
+        return bytes32Storage[_key];
+    }
+
+    /// @param _key The key for the record
+    function getBool(bytes32 _key) external view returns (bool) {
+        return boolStorage[_key];
+    }
+
+    /// @param _key The key for the record
+    function getInt(bytes32 _key) external view returns (int) {
+        return intStorage[_key];
     }
 }
