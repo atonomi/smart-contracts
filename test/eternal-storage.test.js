ab9bc2abaabe2ce4cd48405c460f495561f3f18f
Fil Capiral <fil.capiral@atonomi.io>
2018-10-16 11:27:54 -0700

    storage tests complete

--- test/eternal-storage.test.js
+++ test/eternal-storage.test.js
@@ -15,7 +15,13 @@ contract('EternalStorage', accounts => {
   })
 
   const testCases = [
-    { name: 'Address', key: 'myaddresskey', value: ctx.actors.alice, deleteValue: '0x0000000000000000000000000000000000000000' }
+    { name: 'Address', key: 'myaddresskey', value: ctx.actors.alice, deleteValue: '0x0000000000000000000000000000000000000000' },
+    { name: 'Uint', key: 'myuintkey', value: 10528, deleteValue: '0' },
+    { name: 'String', key: 'mystringkey', value: 'fil', deleteValue: '' },
+    { name: 'Bytes', key: 'mybyteskey', value: 0x123456, deleteValue: '0x' },
+    { name: 'Bytes32', key: 'mybytes32key', value: 0x1234567890, deleteValue: '0x0000000000000000000000000000000000000000000000000000000000000000' },
+    { name: 'Bool', key: 'myboolkey', value: true, deleteValue: 'false' },
+    { name: 'Int', key: 'myintkey', value: 82501, deleteValue: '0' },
   ]
   testCases.forEach((testCase) => {
     describe(testCase.name, () => {
@@ -28,13 +34,19 @@ contract('EternalStorage', accounts => {
 
       it('can set', async () => {
         const actual = await ctx.contracts.storage[`get${testCase.name}`](key)
-        expect(actual).to.be.equal(value)
+        let stringValue = value.toString()
+        if (testCase.name === 'Bytes') {
+          stringValue = '0x' + value.toString(16)
+        } else if (testCase.name === 'Bytes32') {
+          stringValue = '0x' + value.toString(16).padEnd(64, '0')
+        }
+        expect(actual.toString()).to.be.equal(stringValue)
       })
 
       it('can delete', async () => {
         await ctx.contracts.storage[`delete${testCase.name}`](key)
         const actual = await ctx.contracts.storage[`get${testCase.name}`](key)
-        expect(actual).to.be.equal(testCase.deleteValue)
+        expect(actual.toString()).to.be.equal(testCase.deleteValue.toString())
       })
     })
   })
