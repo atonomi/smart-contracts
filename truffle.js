d83a704643eb44c5d25dfd7fd8d9c5460188c87d
Fil Capiral <fil.capiral@atonomi.io>
2018-10-18 11:21:04 -0700

    add local network for kovan deploy

--- truffle.js
+++ truffle.js
@@ -24,6 +24,11 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*'
+    },
+    local: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*' // Match any network id
     }
   }
 }
