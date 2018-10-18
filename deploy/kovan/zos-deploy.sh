66254679d192512e5d90d7b1225c74d4399ad07c
Fil Capiral <fil.capiral@atonomi.io>
2018-10-18 12:39:41 -0700

    all proxies deployed

--- deploy/kovan/zos-deploy.sh
+++ deploy/kovan/zos-deploy.sh
@@ -5,4 +5,11 @@ REPREWARD=125000000000000000
 REPSHARE=20
 BLOCKTHRES=5760
 STORAGE=0xd1bdb6955d76fcac648ccfde16506c30dd21f80c
+TOKEN=0xe66254d9560c2d030ca5c3439c5d6b58061dd6f7
+SETTINGSPROXY=0xdce9635a51a76e2b17da1fd4b249ccb83490767d
+
 zos create NetworkSettings --init initialize --args $OWNER,$REGFEE,$ACTFEE,$REPREWARD,$REPSHARE,$BLOCKTHRES,$STORAGE  --network local --verbose
+zos create TokenPool --init initialize --args $STORAGE --network local --verbose
+zos create ReputationManager --init initialize --args $OWNER,$STORAGE,$TOKEN --network local --verbose
+zos create NetworkMemberManager --init initialize --args $OWNER,$STORAGE --network local --verbose
+zos create DeviceManager --init initialize --args $OWNER,$STORAGE,$TOKEN,$SETTINGSPROXY --network local --verbose
